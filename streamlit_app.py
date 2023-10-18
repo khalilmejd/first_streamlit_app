d65c45621a8911cf403c3f9f1eeb5ae9d98f090b
khalilmejd <148076034+khalilmejd@users.noreply.github.com>
2023-10-18 16:48:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 # Display the response from the FruityVice API
 streamlit.text(fruityvice_response)
 
-#New Section to disply fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+
